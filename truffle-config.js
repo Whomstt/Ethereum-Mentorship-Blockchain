f856e7d5c0897628960e8a752995d01041a1044f
Cathal <cathalcrowepatrick@gmail.com>
2024-12-02 09:37:06 +0000

    add balance to node1

--- truffle-config.js
+++ truffle-config.js
@@ -4,7 +4,7 @@ module.exports = {
       host: "127.0.0.1", // Local Geth node
       port: 8545, // Default Geth RPC port
       network_id: "12345", // Match any network id
-      gas: 6721975, // Set the gas limit
+      gas: 5000000, // Set the gas limit
       gasPrice: 20000000000, // Set the gas price (in Wei)
       from: "0xe508c1571ccc27ac15c7f8793112a2b35776a778", // Set the sender address
     },
